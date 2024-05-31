2c40f099aaa93bed4e2635bf776d02eacfd9cf57
sksmagr23 <sakshamag34@gmail.com>
2024-05-31 20:06:45 +0530

    error done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,41 +1,18 @@
 // backend/index.js
-<<<<<<< HEAD
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
-
-dotenv.config();
-=======
 import express, { json } from 'express';
 import { connect } from 'mongoose';
 import cors from 'cors';
 import { config } from 'dotenv';
 
 config();
->>>>>>> upstream/master
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-<<<<<<< HEAD
-app.use(express.json());
-
-mongoose
-    .connect(process.env.MONGO_URI, {
-=======
 app.use(json());
 
-<<<<<<< HEAD:backend/src/index.js
-connect(process.env.MONGO_URI, {
->>>>>>> upstream/master
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-=======
 connect(process.env.MONGO_URI!)
->>>>>>> upstream/master:backend/src/index.ts
     .then(() => console.log('MongoDB connected'))
     .catch((err) => console.error('Could not connect to MongoDB', err));
 

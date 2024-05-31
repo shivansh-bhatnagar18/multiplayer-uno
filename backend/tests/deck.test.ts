ef2dc47626c6f8457c6c1629748da42c4af09d0b
Divyansh Seth <divyanshseth08@gmail.com>
2024-06-01 00:27:48 +0530

    deck: Implemented Fisher-Yates shuffling algorithm to shuffle the deck Fixes: #2

--- backend/tests/deck.test.ts
+++ backend/tests/deck.test.ts
@@ -1,7 +1,28 @@
-import { makeCard } from '../uno-game-engine/deck';
+import { shuffle, makeCard } from '../uno-game-engine/deck';
 describe('testing deck.ts', () => {
     test('makeCard', () => {
         const card = makeCard('number', 'blue', '3');
         expect(card.color).toBe('blue');
+    });
+});
+describe('shuffle function', () => {
+    test('should change order of elements', () => {
+        // Create a mock deck
+        const deck = [
+            makeCard('number', 'red', '1'),
+            makeCard('number', 'blue', '2'),
+            makeCard('number', 'red', '1'),
+            makeCard('number', 'yellow', '1'),
+        ];
+
+        // Create a copy of the deck for comparison
+        const originalDeck = [...deck];
+        shuffle(deck);
+
+        // Check that the order of elements has changed
+        const orderChanged = deck.some(
+            (card, index) => card !== originalDeck[index]
+        );
+        expect(orderChanged).toBe(true);
     });
 });

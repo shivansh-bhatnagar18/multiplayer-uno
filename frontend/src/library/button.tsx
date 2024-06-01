ca7c999b5480aa7d7668f84bafd73cf0af42c682
ritwik-69 <72665321+ritwik-69@users.noreply.github.com>
2024-06-01 18:03:04 +0530

    client: Created the button component
    This commit creates a button component which is reusable and customizable like mentioned in the issue
    Note:Things like hover effect,focus etc. has still not been implemented as it was not mentioned in the issue
    Update:updated circles as pills and gave borderColor default value of
    textColor
    
    fixes:#20

--- frontend/src/library/button.tsx
+++ frontend/src/library/button.tsx
@@ -8,10 +8,49 @@
 type ButtonProps = {
     onClick: () => void;
     children: React.ReactNode;
+    size?:'small' |'medium' | 'large';
+    textColor?: string;
+    bgColor?: string;
+    borderColor?: string;
+    shape?:'rectangle' | 'rounded' | 'pills';
+
 };
 
-function Button({ onClick, children }: ButtonProps) {
-    return <button onClick={onClick}>{children}</button>;
+
+
+function Button({ children, onClick,size="medium",bgColor="bg-blue-500",textColor="text-white",borderColor=textColor,shape }: ButtonProps) {
+
+const getSizeClass = () => {
+    switch (size) {
+        case 'small':
+            return 'px-2 py-1 text-sm';
+        case 'large':
+            return 'px-4 py-2 text-lg';
+        case 'medium':
+            return 'px-3 py-2 text-base';
+        default:
+            return 'px-3 py-2 text-base';
+    }
+};
+
+const getShapeClass = () => {
+    switch (shape) {
+        case 'rounded':
+            return 'rounded-lg';
+        case 'rectangle':
+            return 'rounded-none';
+        case 'pills':
+            return 'rounded-full';
+        default:
+            return 'rounded-lg';
+    }
+};
+
+const shapeClass = getShapeClass();
+const sizeClass = getSizeClass();
+
+
+    return <button onClick={onClick} className={`${bgColor} ${textColor} border ${borderColor} ${sizeClass} ${shapeClass} `}>{children}</button>;
 }
 
 export default Button;

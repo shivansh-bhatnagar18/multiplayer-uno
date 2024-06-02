ca29e4ad7ff423edea3ae965e2e0fd7af25b8eb4
asmb123 <asmit.barua.phe23@itbhu.ac.in>
2024-06-02 17:05:27 +0530

    Made an error page

--- frontend/src/pages/Error.tsx
+++ frontend/src/pages/Error.tsx
@@ -2,10 +2,13 @@ import { Link } from 'react-router-dom';
 
 function Error() {
     return (
-        <div>
-            This page does not exist!
-            <Link to="home">Go back to homepage</Link>
-        </div>
+        <body className="bg-[#1b1929] min-h-screen flex flex-col justify-center items-center p-4">
+
+            <div className="text-6xl md:text-4xl lg:text-6xl font-semibold text-[#b77fef]">Error 404!</div>
+            <div className="text-8xl md:text-6xl lg:text-8xl font-semibold text-[#b77fef]">Lost in the shuffle</div>
+            <div className="text-2xl md:text-xl lg:text-2xl font-medium text-[#d4c3e4] text-center">This page seems to be missing from the deck</div>
+            <Link to="/" className="mt-4"><button className="bg-[#6d4c8d] text-[#d4c3e4] font-medium py-2 px-4 rounded">Click here for Homepage</button></Link>
+        </body>
     );
 }
 

3c7dc8c7114a7513f62441f0e5a1997d119f2561
sksmagr23 <sakshamag34@gmail.com>
2024-06-07 11:06:08 +0530

    frontend: Added a customizable heading component
    I have created a highly customizable and reusable
    header component in src/library, and the styling is done using tailwind
    css and added various input fields as props.
    
    Fixes: #67

--- frontend/src/library/heading.tsx
+++ frontend/src/library/heading.tsx
@@ -1,13 +1,33 @@
+import React from 'react';
+
 type InputProps = {
-    name?: string;
+    text: string;
+    fontSize?: string;
+    fontWeight?: string;
+    fontStyle?: string;
+    textColor?: string;
+    textAlign?: string;
+    className?: string;
 };
 
-const heading = ({ name }: InputProps) => {
+const Heading: React.FC<InputProps> = ({
+    text,
+    fontSize = 'text-sm', // default value
+    fontWeight = 'font-normal', // default value
+    fontStyle = 'font-sans', // default value
+    textColor = 'text-black', // default value
+    textAlign = 'text-center', // default value
+    className = '', // additional custom classes
+}) => {
     return (
         <div>
-            <h1 className="text-white text-3xl font-bold font-serif">{name}</h1>
+            <h1
+                className={`${fontSize} ${fontWeight} ${fontStyle} ${textColor} ${textAlign} ${className}`}
+            >
+                {text}
+            </h1>
         </div>
     );
 };
 
-export default heading;
+export default Heading;

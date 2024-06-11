e089d97c7b2e238f3433bf1eb957e136931f5909
Akshat Bhatt <akshat.bhatt.ece23@itbhu.ac.in>
2024-06-11 19:39:32 +0530

    issue #81 fixed

--- backend/src/controllers/gameControllers.ts
+++ backend/src/controllers/gameControllers.ts
@@ -1,20 +1,23 @@
+import { Request, Response } from 'express';
 import { getAllClients, scheduleSend } from '../eventRecipients';
 
-async function handleEvent(req, res) {
+type ClientId = string;
+
+async function handleEvent(req:Request, res:Response) {
     const event = req.body;
 
     const clientIds = getAllClients();
 
     const eligibleClientIds = filterEligibleClients(clientIds);
 
-    eligibleClientIds.forEach((clientId) => {
+    eligibleClientIds.forEach((clientId:ClientId) => {
         scheduleSend(clientId, event);
     });
 
     res.status(200).send({ message: 'Event propagated to clients.' });
 }
 
-function filterEligibleClients(clientIds) {
+function filterEligibleClients(clientIds:ClientId[]) {
     return clientIds;
 }
 

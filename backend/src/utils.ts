edfab64fc7eadd0c291b6dc043559b0d18dd4652
Kislay <kislayudbhav.verma.cd.eee22@itbhu.ac.in>
2024-06-26 19:01:18 +0530

    utils: Add function to map card to its image name.
    The function will be used during rendering the cards,
    where we require the filenames of the images corresponding
    to each variant of the UNO card.

--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -1,5 +1,6 @@
 import { Response } from 'express';
 import { AuthRequest } from './middlewares/authMiddleware';
+import { CardColor, UNOCard } from './types';
 
 export type ControllerFunction = (
     req: AuthRequest,
@@ -14,4 +15,44 @@ export function catchError(fn: ControllerFunction): ControllerFunction {
             res.status(500).json({ error: (error as Error).message });
         }
     };
+}
+
+export function getCardImageName(card: UNOCard): string {
+    function getColorAbbreviation(color: CardColor): string {
+        switch (color) {
+            case 'red':
+                return 'r';
+            case 'blue':
+                return 'b';
+            case 'green':
+                return 'g';
+            case 'yellow':
+                return 'o';
+            default:
+                return '';
+        }
+    }
+    if (card.type === 'wild') {
+        if (card.value === 'colchange') {
+            return 'CC';
+        } else {
+            return 'P4';
+        }
+    } else if (card.type === 'special') {
+        let value;
+        switch (card.value) {
+            case 'skip':
+                value = 'r';
+                break;
+            case 'reverse':
+                value = 'x';
+                break;
+            case 'draw2':
+                value = 'p2';
+                break;
+        }
+        return `${getColorAbbreviation(card.color)}${value}`;
+    } else {
+        return `${getColorAbbreviation(card.color)}${card.value}`;
+    }
 }
